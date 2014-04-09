00a7470afd61f46e7b401ffb3ac3f71b1a93812e
laborautonomo <fabio@laborautonomo.org>
2014-04-09 14:49:09 -0300

    [chg] add comment "uncomment this line to use your bookmarks"

--- get-bookmarks.php
+++ get-bookmarks.php
@@ -2,4 +2,10 @@
 require_once 'archive-api-client.php';
 
 $api = new ArchiveApiClient();
+
+/*
+ * uncomment this line to use your bookmarks
+ * */
+//$api->set_my_bookmarks('http://archive.org/bookmarks/YourAccount');
+
 exit($api->get_my_bookmarks());
